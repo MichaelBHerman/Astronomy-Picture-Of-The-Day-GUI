868f1a6289cc7a9e6c672ea287a03811d2c97567
michaelherman <michaelbherman@gmail.com>
2023-08-14 13:33:08 -0600

    Initial Commit

---
+++ src/main/java/dev/michael/Layout.java
@@ -0,0 +1,151 @@
+package dev.michael;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class Layout extends JFrame implements ActionListener{
+
+    public Layout(String title) throws IOException {
+        super(title);
+        this.setSize(850, 800);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getRootPane().setBorder(BorderFactory.createMatteBorder(4,4,4,4, Color.white));
+
+        //OKHttp instance
+        OKHttp okHttp = new OKHttp();
+
+        //JButtons
+        JButton prev = new JButton("Prev");
+        JButton next = new JButton("Next");
+
+        //Colors
+        Color nasaBlue = new Color(10, 61, 145);
+        Color nasaRed = new Color(252, 61, 34);
+
+        //Container
+        Container container = this.getContentPane();
+        container.setLayout(new BorderLayout(8,6));
+        container.setBackground(nasaBlue);
+
+
+        //Top Panel
+        JPanel topPanel = new JPanel();
+        topPanel.setBorder(new LineBorder(Color.white, 3));
+        topPanel.setBackground(nasaBlue);
+        topPanel.setLayout(new FlowLayout(5));
+        topPanel.add(prev);
+        topPanel.add(next);
+
+        //JTextArea for image title
+        JTextArea title1 = new JTextArea();
+        title1.setBackground(nasaBlue);
+        title1.setSize(100,100);
+        title1.setForeground(Color.white);
+        title1.setText("                           " + okHttp.getDate() + " : " + okHttp.getTitle());
+        topPanel.add(title1);
+
+        container.add(topPanel, BorderLayout.NORTH);
+
+        //JLabel for imageIcon
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        label.setBorder(new LineBorder((nasaRed), 3));
+        label.setBounds(100,100,580,600);
+
+        //ImageIcon for APOD
+        String apod = okHttp.getHdUrl();
+        URL apodUrl = new URL(apod);
+        BufferedImage urlImage = ImageIO.read(apodUrl);
+        ImageIcon icon = new ImageIcon(urlImage);
+        //Scaling the image
+        Image img = icon.getImage();
+        Image imgScale = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon scaled = new ImageIcon(imgScale);
+        label.setIcon(scaled);
+
+        container.add(label);
+
+
+        //Botton Panel
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout(3));
+
+        //Bottom Text Area for Image Explanation
+        JTextArea text = new JTextArea();
+        text.setLineWrap(true);
+        text.setWrapStyleWord(true);
+        text.setSize(800, 300);
+        text.setText(okHttp.getExplanation());
+        text.setFont(Font.getFont("arial"));
+        bottomPanel.add(text);
+        bottomPanel.setBorder(new LineBorder(nasaBlue, 5));
+        container.add(bottomPanel, BorderLayout.SOUTH);
+
+        //prev and next button action listeners
+        prev.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String[] newData = okHttp.getPrevDateData();
+                    URL newUrl = new URL(newData[0]);
+                    BufferedImage urlImage = ImageIO.read(newUrl);
+                    ImageIcon icon = new ImageIcon(urlImage);
+                    label.setIcon(icon);
+                    Image img = icon.getImage();
+                    Image imgScale = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+                    ImageIcon scaled = new ImageIcon(imgScale);
+                    label.setIcon(scaled);
+                    //title
+                    title1.setText("                           " + newData[3] + " : " + newData[1]);
+                    //explanation
+                    text.setText(newData[2]);
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String[] newData = okHttp.getNextDateData();
+                    URL newUrl = new URL(newData[0]);
+                    BufferedImage urlImage = ImageIO.read(newUrl);
+                    ImageIcon icon = new ImageIcon(urlImage);
+                    label.setIcon(icon);
+                    Image img = icon.getImage();
+                    Image imgScale = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+                    ImageIcon scaled = new ImageIcon(imgScale);
+                    label.setIcon(scaled);
+                    //title
+                    title1.setText("                              " + newData[3] + " : " + newData[1]);
+                    //explanation
+                    text.setText(newData[2]);
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+
+    }
+    public static void main(String[] args) throws IOException {
+        Layout layout = new Layout("NASA ASTRONOMY PICTURE OF THE DAY");
+        layout.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/main/java/dev/michael/OKHttp.java
@@ -0,0 +1,93 @@
+package dev.michael;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class OKHttp {
+    final ObjectMapper mapper = new ObjectMapper();
+    public OkHttpClient client = new OkHttpClient();
+    public LocalDateTime ldt = LocalDateTime.now();
+    public int minusDateCount = 1;
+
+    Request request = new Request.Builder()
+            .url("https://api.nasa.gov/planetary/apod?api_key=Emii5aEQxDLySfoUKSX1XKXTBNiPp5bMXCdeXr5c")
+            .build(); // defaults to GET
+    Response response = client.newCall(request).execute();
+    APOD apod = mapper.readValue(response.body().string(), APOD.class);
+
+
+    public OKHttp() throws IOException {
+    }
+
+    public String getHdUrl() {
+        return apod.hdUrl;
+    }
+
+    public String getExplanation() {
+        return apod.explanation;
+    }
+
+    public String getTitle() {
+        return apod.title;
+    }
+
+    public String getDate() {
+        return apod.date;
+    }
+
+    public String getFormattedPrevDate(LocalDateTime date) {
+
+        LocalDateTime previousDate = date.minusDays(minusDateCount);
+        minusDateCount++;
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)
+                .format(previousDate);
+    }
+
+    public String getFormattedNextDate(LocalDateTime date) {
+
+        LocalDateTime previousDate = date.minusDays(minusDateCount);
+        minusDateCount--;
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH)
+                .format(previousDate);
+    }
+    //TODO: refactor this so that it concatenates the correct method depending on next or previous button press
+    public String[] getPrevDateData() throws IOException {
+        //local date time
+
+        String dateRequest = "https://api.nasa.gov/planetary/apod?&api_key=Emii5aEQxDLySfoUKSX1XKXTBNiPp5bMXCdeXr5c&date=" + getFormattedPrevDate(ldt);
+        Request request = new Request.Builder()
+                .url(dateRequest)
+                .build(); // defaults to GET
+        Response response = client.newCall(request).execute();
+        APOD apod = mapper.readValue(response.body().string(), APOD.class);
+        String[] newData = new String[4];
+        newData[0] = apod.hdUrl;
+        newData[1] = apod.title;
+        newData[2] = apod.explanation;
+        newData[3] = apod.date;
+        return newData;
+    }
+
+    public String[] getNextDateData() throws IOException {
+        //local date time
+
+        String dateRequest = "https://api.nasa.gov/planetary/apod?&api_key=Emii5aEQxDLySfoUKSX1XKXTBNiPp5bMXCdeXr5c&date=" + getFormattedNextDate(ldt);
+        Request request = new Request.Builder()
+                .url(dateRequest)
+                .build(); // defaults to GET
+        Response response = client.newCall(request).execute();
+        APOD apod = mapper.readValue(response.body().string(), APOD.class);
+        String[] newData = new String[4];
+        newData[0] = apod.hdUrl;
+        newData[1] = apod.title;
+        newData[2] = apod.explanation;
+        newData[3] = apod.date;
+        return newData;
+    }
+}
